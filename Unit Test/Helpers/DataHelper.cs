6d4361610a633d8abcc63373661475240f00e549
rakesh <rakesh_anvekar@gmail.com>
2024-10-28 18:10:50 +0530

    unit test cases

---
+++ Unit Test/Helpers/DataHelper.cs
@@ -0,0 +1,15 @@
+﻿
+
+using ProductsApi.Models.Product;
+using ProductsApi.Models.User;
+
+namespace Unit_Test.Helpers;
+
+internal sealed class DataHelper
+{
+    public List<Product> products { get; set; } = new()
+    {
+        new Product{ Id = 1,ProductName="Test Product",Price=10.00m,CategoryType="Eletric"},
+        new Product{ Id = 2,ProductName="Test Product 2",Price=10.00m,CategoryType="Eletric"},
+    };  
+}
---
+++ Unit Test/Helpers/InstanceHelper.cs
@@ -0,0 +1,50 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using ProductsApi.Models.User;
+using ProductsApi.Repositories;
+using ProductsApi.Repositories.Interfaces;
+
+namespace Unit_Test.Helpers;
+
+internal sealed class InstanceHelper
+{
+    #region fields
+    private readonly DataHelper _dataHelper;
+    private readonly ProductRepository? _productRepository;
+    private readonly UserRepository? _userRepository;
+    #endregion
+
+    #region constructor
+    public InstanceHelper(DataHelper dataHelper)
+    {
+        _dataHelper = dataHelper;
+        
+
+    }
+    #endregion
+
+    #region methods
+    public async Task BuildAsync(CancellationToken cancellationToken)
+    {
+        var builder = WebApplication.CreateBuilder(Array.Empty<string>());
+        builder.Services.AddSingleton(await GetProductRepositoryAsync());
+       
+    }
+    private async Task<IProductRepository> GetProductRepositoryAsync()
+    {
+        if (_productRepository != null)
+        {
+            return _productRepository;
+        }
+        var mock = new Mock<IProductRepository>();
+        mock.Setup(static m => m.GetAsync(It.IsAny<CancellationToken>())).ReturnsAsync((CancellationToken _) => _dataHelper.products);
+        return await Task.FromResult(mock.Object);
+
+        #endregion
+    }
+
+   
+    }
+
