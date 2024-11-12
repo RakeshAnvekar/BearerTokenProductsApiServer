ca7a51fd167271557b9376be20943faf6608177a
rakesh <rakesh_anvekar@gmail.com>
2024-11-12 18:45:51 +0530

    adding ProductLogicTests

---
+++ Unit Test/Tests/BusinessLogicTests/ProductLogicTests.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using ProductsApi.BusinessLogic;
+using ProductsApi.BusinessLogic.Interfaces;
+using ProductsApi.Controllers;
+using ProductsApi.Models.Product;
+using ProductsApi.Repositories.Interfaces;
+using Unit_Test.Helpers;
+
+namespace Unit_Test.Tests.BusinessLogicTests;
+
+public sealed class ProductLogicTests
+{
+    private readonly Mock<IProductRepository> _productRepositorycMock;
+    private readonly IProductLogic _productLogic;
+
+    public ProductLogicTests()
+    {
+        _productRepositorycMock= new Mock<IProductRepository>();
+        _productLogic = new ProductLogic(_productRepositorycMock.Object);
+
+    }
+    [Test]
+    public async Task GetAsync_SuccessfullyReturnsAllProducts()
+    {
+        // Arrange
+        DataHelper dataHelper = new DataHelper();
+       
+       _productRepositorycMock.Setup(repo => repo.GetAsync(CancellationToken.None)).ReturnsAsync(dataHelper.products);
+        var result = await _productLogic.GetAsync(CancellationToken.None);
+        Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(result, dataHelper.products);
+    }
+    [Test]
+    public async Task GetAsync_SuccessfullyReturnsAllProductsById()
+    {
+        // Arrange
+        DataHelper dataHelper = new DataHelper();
+
+        _productRepositorycMock.Setup(repo => repo.GetAsync(1, CancellationToken.None)).ReturnsAsync(dataHelper.products[1]);
+       
+        var result = await _productLogic.GetAsync(1,CancellationToken.None);
+        Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(result);
+        Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(result, dataHelper.products[1]);
+    }
+
+}
--- Unit Test/obj/Debug/net8.0/Unit Test.AssemblyInfo.cs
+++ Unit Test/obj/Debug/net8.0/Unit Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unit Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabe84f2658a476e89d3e97abad397dd188ffd6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5629ed3fb8c567c9528dd1b21e604539a677c5b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unit Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unit Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
